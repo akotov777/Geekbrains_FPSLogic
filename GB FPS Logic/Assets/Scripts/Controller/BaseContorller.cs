038c241de5d76a950b0590f7d64a66b58a021d70
AntonKotov <akotov777@gmail.ocm>
2020-05-02 19:20:43 +0500

    Lesson 1. First scene and FlashlightUI bar

---
+++ GB FPS Logic/Assets/Scripts/Controller/BaseContorller.cs
@@ -0,0 +1,37 @@
+﻿namespace FPSLogic
+{
+    public abstract class BaseController
+    {
+        #region Properties
+
+        public bool IsActive { get; private set; }
+
+        #endregion
+
+
+        #region Methods
+
+        public virtual void On()
+        {
+            On(null);
+        }
+
+        public virtual void On(params BaseSceneObject[] objects)
+        {
+            IsActive = true;
+        }
+
+        public virtual void Off()
+        {
+            IsActive = false;
+        }
+
+        public void Switch()
+        {
+            if (!IsActive) On();
+            else Off();
+        }
+
+        #endregion
+    }
+}
---
+++ GB FPS Logic/Assets/Scripts/Controller/GameController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+
+namespace FPSLogic
+{
+    public sealed class GameController : MonoBehaviour
+    {
+        #region Fields
+
+        private Controllers _controllers;
+
+        #endregion
+
+
+        #region UnityMethods
+
+        private void Start()
+        {
+            _controllers = new Controllers();
+            _controllers.Initialization();
+        }
+
+        private void Update()
+        {
+            for (var i = 0; i < _controllers.Length; i++)
+            {
+                _controllers[i].Execute();
+            }
+        }
+
+        #endregion
+    }
+}
